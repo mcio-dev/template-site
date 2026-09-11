89f307932011465fcce307dc0c7c16dd9de6cf4c
MrXiaoM <coolxiaom95@gmail.com>
2026-09-11 10:32:18 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ const versions = {
     // https://github.com/GradleUp/shadow/releases
     shadowJar: '9.3.0',
     // https://github.com/MrXiaoM/PluginBase/releases
-    PluginBase: '1.8.0',
+    PluginBase: '1.8.1',
     // https://github.com/PlaceholderAPI/PlaceholderAPI/releases
     PlaceholderAPI: '2.12.2',
     // https://modrinth.com/plugin/playerpoints/versions
